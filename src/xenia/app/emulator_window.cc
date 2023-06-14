c846a6c2d319e20ad7cc96b8deb2330232391338
sirmangler <thecatsmangler@gmail.com>
2023-06-14 17:55:08 +0100

    Add version to About tab

--- src/xenia/app/emulator_window.cc
+++ src/xenia/app/emulator_window.cc
@@ -2452,7 +2452,7 @@ void EmulatorWindow::WinRTFrontendDialog::OnDraw(ImGuiIO& io) {
 
       if (ImGui::BeginTabItem("About", nullptr)) {
         ImGui::TextWrapped(
-            "Xenia UWP - A fork of Xenia introducing Xbox support and a big "
+            "Xenia UWP 1.1.3 - A fork of Xenia introducing Xbox support and a big "
             "picture frontend.\n"
             "Xenia's Website: https://xenia.jp/\n"
             "Xenia's Patreon: https://www.patreon.com/xenia_project\n\n"
